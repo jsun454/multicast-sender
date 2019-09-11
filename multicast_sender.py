0ef6f6e81b4b61999f587b99219f5c7f33f53492
jsun454 <jsun46@umail.ucsb.edu>
2019-09-10 23:48:32 -0700

    Multicast data is received by TV app

--- multicast_sender.py
+++ multicast_sender.py
@@ -3,4 +3,12 @@ import socket
 multicast_group = '239.0.0.1'
 multicast_port = 8888
 
-# TODO
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2) # maybe default value 1 is fine but use 2 for now
+
+test = [1, 3, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+test2 = 'JohnnyDepp'
+test3 = [0, 1]
+
+data = bytes(test) + bytes(test2, 'utf-8') + bytes(test3)
+sock.sendto(data, (multicast_group, multicast_port))
