7a2e3958dfea9f95b9051a17d00ccfb890855af7
jsun454 <jsun46@umail.ucsb.edu>
2019-09-14 14:48:23 -0700

    Use thread instead of asyncio

--- multicast_sender.py
+++ multicast_sender.py
@@ -1,3 +1,4 @@
+'''
 import socket
 import asyncio
 
@@ -16,15 +17,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
 #     data = bytes(test) + bytes(test2, 'utf-8') + bytes(test3)
 #     sock.sendto(data, (multicast_group, multicast_port))
+
 async def main():
+    # tasks = []
     for x in range(1, 11):
         asyncio.get_event_loop().create_task(start_kart(x, x))
+        # task = asyncio.get_event_loop().create_task(start_kart(x, x))
+        # tasks.append(task)
+
+    # for task in tasks:
+    #     await task
 
 async def start_kart(delay, kart):
     # await asyncio.sleep(delay)
     prevLap = 0
     elapsed = 0
-    while(True):
+    while True:
         data_part_1 = [kart, 2, 1, 3] # kartNO, status, currLap, totalLaps
         data_part_2 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # prevLap
         data_part_3 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # bestLap
@@ -37,6 +45,68 @@ async def start_kart(delay, kart):
             prevLap = 5000 - elapsed // 5000
         # await asyncio.sleep(0.1)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
 loop.close()
+'''
+
+'''
+from multiprocessing import Process
+from time import sleep
+import socket
+
+def start_kart(delay, kart):
+    multicast_group = '239.0.0.1'
+    multicast_port = 8888
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    prevLap = 0
+    elapsed = 0
+    while True:
+        data_part_1 = [kart, 2, 1, 3] # kartNO, status, currLap, totalLaps
+        data_part_2 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # prevLap
+        data_part_3 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # bestLap
+        data_part_4 = ('Racer ' + str(kart) + '   ')[0:10] # name
+        data_part_5 = [0, 1] # checkSum, mode
+        data = bytes(data_part_1) + bytes(data_part_2) + bytes(data_part_3) + bytes(data_part_4, 'utf-8') + bytes(data_part_5)
+        sock.sendto(data, (multicast_group, multicast_port))
+        elapsed += 100
+        if(elapsed % 5000):
+            prevLap = 5000 - elapsed // 5000
+        sleep(1)
+
+if __name__ == '__main__':
+    for x in range(1, 11):
+        Process(target=start_kart, args=(x, x)).start()
+'''
+
+from threading import Thread
+from time import sleep
+import socket
+
+multicast_group = '239.0.0.1'
+multicast_port = 8888
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+def start_kart(delay, kart):
+    prevLap = 0
+    elapsed = 0
+    # sleep(delay)
+    while True:
+        data_part_1 = [kart, 2, 1, 3] # kartNO, status, currLap, totalLaps
+        data_part_2 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # prevLap
+        data_part_3 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # bestLap
+        data_part_4 = ('Racer ' + str(kart) + '   ')[0:10] # name
+        data_part_5 = [0, 1] # checkSum, mode
+        data = bytes(data_part_1) + bytes(data_part_2) + bytes(data_part_3) + bytes(data_part_4, 'utf-8') + bytes(data_part_5)
+        sock.sendto(data, (multicast_group, multicast_port))
+        elapsed += 100
+        if(elapsed % 5000):
+            prevLap = 5000 - elapsed // 5000
+        sleep(0.1)
+
+def main():
+    for x in range(1, 2):
+        Thread(target=start_kart, args=(x,x)).start()
+
+if __name__ == '__main__':
+    main()
