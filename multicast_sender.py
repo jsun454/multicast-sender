84e1030d8a929154f249694a238f0851a4270742
jsun454 <jsun46@umail.ucsb.edu>
2019-09-18 01:51:39 -0700

    Changed multicast data packet structure

--- multicast_sender.py
+++ multicast_sender.py
@@ -90,22 +90,23 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 def start_kart(delay, kart):
     prevLap = 0
     elapsed = 0
+    lap = 1
     # sleep(delay)
     while True:
-        data_part_1 = [kart, 2, 1, 3] # kartNO, status, currLap, totalLaps
-        data_part_2 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # prevLap
-        data_part_3 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # bestLap
-
+        data_part_1 = [kart, 2, lap, 3] # kartNO, status, currLap, totalLaps
+        # data_part_2 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # prevLap
+        # data_part_3 = [0, ((prevLap // 256) // 256) % 256, (prevLap // 256) % 256, prevLap % 256] # bestLap
         data_part_2 = prevLap.to_bytes(4, 'big') # prevLap
-        data_part_3 = prevLap.to_bytes(4, 'big') # bestLap
-
-        data_part_4 = ('Racer ' + str(kart) + '   ')[0:10] # name
-        data_part_5 = [0, 1] # checkSum, mode
-        data = bytes(data_part_1) + bytes(data_part_2) + bytes(data_part_3) + bytes(data_part_4, 'utf-8') + bytes(data_part_5)
+        data_part_3 = elapsed.to_bytes(4, 'big') # timeSpent
+        data_part_4 = (1000*(60*5+11)+888).to_bytes(4, 'big') # totalTime
+        data_part_5 = ('Racer ' + str(kart) + '   ')[0:10] # name
+        data_part_6 = [0, kart%2] # checkSum, mode
+        data = bytes(data_part_1) + data_part_2 + data_part_3 + data_part_4 + bytes(data_part_5, 'utf-8') + bytes(data_part_6)
         sock.sendto(data, (multicast_group, multicast_port))
         elapsed += 100
         if(elapsed % 5000):
             prevLap = 5000 - elapsed // 5000
+        lap = elapsed // 5000 + 1
         sleep(0.1)
 
 def main():
